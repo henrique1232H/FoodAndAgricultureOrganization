d43a0501f7ff99e2d7944f6bea7752acbb2ee47e
ICEBERG776 <henriqueau111@gmail.com>
2023-03-04 20:03:51 -0300

    modified:   src/components/section1/style.css 	modified:   src/components/section5/style.css 	modified:   src/components/section6/index.jsx 	modified:   src/components/section6/style.css

--- src/components/section6/index.jsx
+++ src/components/section6/index.jsx
@@ -1,19 +1,43 @@
 import "./style.css"
 
+import cow from "../../assets/svg/animals/cowIcon.svg"
+
 export function SectionSix() {
 
     return (
         <section className="containerSectionSix">
 
-            <div className="">
+            <div className="dashed">
                 
                 <h1>Consumo de água</h1>
 
-                <div className="graphicCarousel">
+                <div className="flexGraphics outline">
                     <div>
-                        <img src="" alt="" />
+                        <img src={cow} alt="" />
+
+                        <table>
+                            <thead>
+                                <td>
+                                    <th>
+                                        <h2>Suino</h2>
+                                    </th>
+                                </td>
+                            </thead>
+
+                            <tbody>
+                                <td>
+                                    <th>Evapotranspiração[%] </th>
+                                </td>
+                            </tbody>
+                        </table>
+
+                    </div>
+
+                    <div>
+                        <h2>tete</h2>
                     </div>
                 </div>
+                
             
             </div>
             
