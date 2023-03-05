3e3f284e13161c4b21d60f7932fefa1812357f40
ICEBERG776 <henriqueau111@gmail.com>
2023-03-04 21:13:15 -0300

    modified:   src/components/section6/index.jsx 	modified:   src/components/section6/style.css

--- src/components/section6/index.jsx
+++ src/components/section6/index.jsx
@@ -1,6 +1,8 @@
 import "./style.css"
 
+import pig from "../../assets/svg/animals/pigIcon.svg"
 import cow from "../../assets/svg/animals/cowIcon.svg"
+import chicken from "../../assets/svg/animals/chickenIcon.svg"
 
 export function SectionSix() {
 
@@ -12,30 +14,75 @@ export function SectionSix() {
                 <h1>Consumo de água</h1>
 
                 <div className="flexGraphics outline">
-                    <div>
-                        <img src={cow} alt="" />
+                    <div className="boxTable">
+                        <img src={pig} alt="" />
 
-                        <table>
-                            <thead>
-                                <td>
-                                    <th>
-                                        <h2>Suino</h2>
-                                    </th>
-                                </td>
-                            </thead>
+                        <div className="table">
+                            <div className="tableTitle">
+                              <h2>Suino</h2>  
+                            </div>
+                            
+                            <div className="tableStats">
 
-                            <tbody>
-                                <td>
-                                    <th>Evapotranspiração[%] </th>
-                                </td>
-                            </tbody>
-                        </table>
+                                <div className="tableText">
+                                    <p>Evapotransipração[%]</p>
+                                    <p>99.7</p>
+                                </div>
+                            </div>
+                            
+
+                            <div className="tableStats">
+                                <div className="tableText">
+                                    <p>Dessedentação[%]</p>
+                                    <p>0.1</p>
+                                </div>
+                            </div>
+
+                            <div className="tableStats">
+                                <div className="tableText">
+                                    <p>Limpeza[%]</p>
+                                    <p>0.2</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+                    <div className="boxTable">
+                        <img src={chicken} alt="" />
+
+                        <div className="table">
+                            <div className="tableTitle">
+                              <h2>Suino</h2>  
+                            </div>
+                            
+                            <div className="tableStats">
+
+                                <div className="tableText">
+                                    <p>Evapotransipração[%]</p>
+                                    <p>99.7</p>
+                                </div>
+                            </div>
+                            
+
+                            <div className="tableStats">
+                                <div className="tableText">
+                                    <p>Dessedentação[%]</p>
+                                    <p>0.1</p>
+                                </div>
+                            </div>
+
+                            <div className="tableStats">
+                                <div className="tableText">
+                                    <p>Limpeza[%]</p>
+                                    <p>0.2</p>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
-                    <div>
-                        <h2>tete</h2>
-                    </div>
+
                 </div>
                 
             
