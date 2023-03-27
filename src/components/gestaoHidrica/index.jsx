3ed573b8982862dceec193695e33c4338093ad52
Henrique Cardoso de Oliveira <henriqueau111@gmail.com>
2023-03-27 12:23:19 -0300

    modified:   src/components/gestaoHidrica/index.jsx 	modified:   src/components/gestaoHidrica/style.css

--- src/components/gestaoHidrica/index.jsx
+++ src/components/gestaoHidrica/index.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import cow from "../../assets/svg/cow.svg";
 import "./style.css"
 
 const Div = styled.div`
@@ -9,25 +8,43 @@ const Div = styled.div`
 const DivText = styled.div`
     height: 100vh;
     background: #fff;
-    width: 40%;
-    border-top-right-radius:  40%;
-    border-bottom-right-radius: 40%;
-    box-shadow: 5px 5px 40px 1px #ccc;
+    width: 30%;
+    border-top-right-radius:  20%;
+    border-bottom-right-radius: 20%;
+    box-shadow: 5px 5px 10px 1px #ccc;
     display: flex;
     flex-direction: column;
     padding-top: 10rem;
 
-
 `
 
 const H3 = styled.h3`
-    height: 20vh;
-    width: 50%;
-    margin-left: 10rem;
+    height: 14vh;
+    width: 60%;
+    margin-left: 7rem;
     text-align: center;
-    font-size: 2.5rem;
+    font-size: 3rem;
 `
 
+const Linha = styled.div`
+    height: 0.2rem;
+    margin-left: 2rem;
+    width: 90%;
+    background: #ccc;
+    margin-bottom: 2rem;
+`
+
+const Paragrafos = styled.div`
+    padding-left: 2rem;
+`
+
+const P = styled.p`
+    font-size: 1.4rem;
+`
+
+const Subtitle = styled.h3`
+    margin-bottom: 3rem;
+`
 
 export default function Agua() {
 
@@ -37,15 +54,25 @@ export default function Agua() {
 
                 
 
-                <H3>Gestão hídrica na produção animal: uso mais eficiente dos recursos</H3>
+                <H3>Gestão hídrica na produção animal</H3>
 
-                <p>•As grande maioria das fazendas de produção animal <span className="weight">NÃO</span> mensuram o consumo de água para a produção animal. </p>
+                <Linha />
 
-                <p>• <span className="weight">NÃO</span> se gerencia o que <span className="weight">NÃO</span> se mede!!</p>
+                <Subtitle>Uso mais eficiente dos recursos</Subtitle>
 
-                <p>•Ao <span className="weight">medir</span> o consumo de água, o <span className="weight"> produtor compreende os valores</span>  e pode implementar <span className="weight">boas práticas</span> para usar os recursos hídricos de forma mais eficiente.</p>
+                <Paragrafos>
 
-                <p> •O  <span className="weight">desconhecimento da demanda hídrica</span> para as atividades pecuárias e eventos de escassez hídrica podem <span className="weight">comprometer a continuidade da atividade</span>, impactando economias e o abastecimento de alimentos.</p>
+
+                <P>•As grande maioria das fazendas de produção animal <span className="weight">NÃO</span> mensuram o consumo de água para a produção animal. </P>
+
+                <P>• <span className="weight">NÃO</span> se gerencia o que <span className="weight">NÃO</span> se mede!!</P>
+
+                <P>•Ao <span className="weight">medir</span> o consumo de água, o <span className="weight"> produtor compreende os valores</span>  e pode implementar <span className="weight">boas práticas</span> para usar os recursos hídricos de forma mais eficiente.</P>
+
+                <P> •O  <span className="weight">desconhecimento da demanda hídrica</span> para as atividades pecuárias e eventos de escassez hídrica podem <span className="weight">comprometer a continuidade da atividade</span>, impactando economias e o abastecimento de alimentos.</P>
+
+                </Paragrafos>
+
 
             </DivText>
 
