cfbeafae6b42294f90bb61983dfe37179a97792b
Henrique Cardoso de Oliveira <henriqueau111@gmail.com>
2023-03-30 15:14:56 -0300

    modified:   src/components/Footer/index.jsx 	modified:   src/components/Footer/style.css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -17,6 +17,29 @@ const Footer = styled.footer`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    height: auto;
+`
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 3rem;
+    background: linear-gradient(#0099ff, #00099F);
+    padding: 4rem 5rem;
+
+    @media screen and (max-width: 40rem) {
+        grid-template-columns: 1fr;
+    }
+    
+`
+
+const P = styled.p`
+    font-size: 1.5rem;
+    color: #fff;
+` 
+
+const Block = styled.div`
+     
 `
 
 export default function SectionTen() {
@@ -25,41 +48,41 @@ export default function SectionTen() {
         <Footer>
 
             
-                <div className="flexSectionTen">
+                <Grid>
 
 
-                    <div className="fixed">
-                        <p>Projeto desenvolvido entre os anos de 2019-2023 através do doutorado da Eng Ambiental Sofia Helena Zanella Carra:
+                    <Block>
+                        <P>Projeto desenvolvido entre os anos de 2019-2023 através do doutorado da Eng Ambiental Sofia Helena Zanella Carra:
                         Avaliação da Produtividade Hídrica da Produção Agrícola e Pecuária em uma Bacia Hidrográfica Experimental Localizada no Sul do Brasil
                         Bolsa de estudos do DAAD (Ref. 91693718)
                         Projeto desenvolvido em parceria entre:
-                        Leibniz-Institut für Agrartechnik und Bioökonomie e.V. (ATB): Dr PD Katrin Drastig</p>
-                    </div>
+                        Leibniz-Institut für Agrartechnik und Bioökonomie e.V. (ATB): Dr PD Katrin Drastig</P>
+                    </Block>
 
-                    <div className="fixed">
-                        <p>Empresa Brasileira de Pesquisa Agropecuária (Embrapa Pecuária Sudeste): Dr Julio Cesar Pascale Palhares
+                    <Block>
+                        <P>Empresa Brasileira de Pesquisa Agropecuária (Embrapa Pecuária Sudeste): Dr Julio Cesar Pascale Palhares
                         Universidade de Caxias do Sul: Dr Vania Elisabete Schneider, Dr Taison Bortolin, Dr Cintia Paese Giacomello
                         EMATER-ASCAR/RS, Unidade Serafina Correa: Eng. Leandro Ebert
-                        Prefeituras municipais de Serafina Correa, União da Serra, Montauri e Guaporé</p>
-                    </div>
+                        Prefeituras municipais de Serafina Correa, União da Serra, Montauri e Guaporé</P>
+                    </Block>
 
-                    <div className="test">
+                    <Block>
                         <h2>Outras referências sobre o tema no Brasil:</h2>
 
-                        <p>Produção animal e recursos hídricos: uso da água nas dimensões quantitativa e qualitativa e cenários regulatórios e de consumo v.3 </p>
+                        <P>Produção animal e recursos hídricos: uso da água nas dimensões quantitativa e qualitativa e cenários regulatórios e de consumo v.3 </P>
 
 
 
-                        <p>Produção animal e recursos hídricos: tecnologias para manejo de resíduos e uso eficiente dos insumos v.2</p>
+                        <P>Produção animal e recursos hídricos: tecnologias para manejo de resíduos e uso eficiente dos insumos v.2</P>
                         <a href="https://www.embrapa.br/busca-de-publicacoes/-/publicacao/1116498/producao-animal-e-recursos-hidricos-tecnologias-para-manejo-de-residuos-e-uso-eficiente-dos-insumos">https://www.embrapa.br/busca-de-publicacoes/-/publicacao/1116498/producao-animal-e-recursos-hidricos-tecnologias-para-manejo-de-residuos-e-uso-eficiente-dos-insumos</a>
 
 
-                        <p>Produção Animal e Recursos Hídricos v.1</p>
+                        <P>Produção Animal e Recursos Hídricos v.1</P>
                         <a href="https://www.embrapa.br/busca-de-publicacoes/-/publicacao/1048070/producao-animal-e-recursos-hidricos">https://www.embrapa.br/busca-de-publicacoes/-/publicacao/1048070/producao-animal-e-recursos-hidricos</a>
 
-                    </div>
+                    </Block>
 
-                </div>
+                </Grid>
 
       
                     <h3>Parceiros</h3>
