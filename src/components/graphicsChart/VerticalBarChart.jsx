1fc9cd68c9490e4e86cedc0cbbdf7577b9b7be73
Henrique Cardoso de Oliveira <henriqueau111@gmail.com>
2023-03-31 16:50:45 -0300

    modified:   src/components/graphicsChart/VerticalBarChart.jsx

--- src/components/graphicsChart/VerticalBarChart.jsx
+++ src/components/graphicsChart/VerticalBarChart.jsx
@@ -39,30 +39,30 @@ import {
     datasets: [
       {
         label: 'Aves',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        data: [120, 80.0, 45.0, 90.0,30.0, 30.0, 20.0, 30.0, 5.0, 40.0, 30.0, 90.0],
         backgroundColor: 'rgba(0, 255, 4, 0.5)',
       },
       {
         label: 'Porco',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        data: [120.0, 149.6, 145.1, 126.0, 101.9, 69.4, 59.7, 64.6, 33.1, 59.5, 61.9, 93.0],
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
       },
 
       {
         label: 'Leite - sistema à pasto',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        data: [80.0, 90.0, 95.0, 80.0, 60.0, 42.0, 40.0, 42.0, 20.0, 40.0, 40.0, 61.9],
         backgroundColor: '#ffee00',
       },
 
       {
         label: 'Leite - sistema semi-conf ',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        data: [70.0, 80.0, 85.0, 70.0 , 50.0, 40.0, 38.0, 40.0, 20.0, 40.0, 43.0, 70.0 ],
         backgroundColor: 'rgba(255, 0, 47, 0.5)',
       },
 
       {
         label: 'Leite - sistema confinado',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 50 })),
+        data: [82.0, 90.0, 85.0, 80.0, 52.0, 45.0, 40.0, 43.0, 20.0, 38.0, 40.0, 59.4  ],
         backgroundColor: 'rgba(13, 0, 255, 0.5)',
       },
     ],
