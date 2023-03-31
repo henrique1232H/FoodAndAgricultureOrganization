7ce20e150f4b8f38c65e2df028d012d7034ce19d
Henrique Cardoso de Oliveira <henriqueau111@gmail.com>
2023-03-31 13:37:13 -0300

    modified:   src/components/Tabelas/index.jsx

--- src/components/Tabelas/index.jsx
+++ src/components/Tabelas/index.jsx
@@ -28,6 +28,7 @@ const Table = styled.table`
   filter: drop-shadow(0px 4px 5px rgba(0, 0, 0, 0.5));
 
   @media screen and (max-width: 40rem) {
+    overflow-x: scroll
     height: 10vh;
     width: 20%;
   }
@@ -61,8 +62,6 @@ const Abc = styled.th`
 const Title = styled.th`
   text-align: center;
   font-size: 2rem;
-
-  
 `;
 
 export default function Tabelas() {
