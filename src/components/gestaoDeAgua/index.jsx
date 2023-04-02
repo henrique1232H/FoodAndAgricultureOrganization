bae6a99fee03d6b5328d879617418d29c6db1d35
Henrique Cardoso de Oliveira <henriqueau111@gmail.com>
2023-04-01 22:16:58 -0300

    modified:   src/components/gestaoDeAgua/index.jsx

--- src/components/gestaoDeAgua/index.jsx
+++ src/components/gestaoDeAgua/index.jsx
@@ -31,19 +31,19 @@ export function Water() {
 
                     <div className="p">
 
-                        <p>Até 2050, a FAO estima que a <span className="weight">agricultura</span> precisará produzir quase <span className="weight">50% mais alimentos, rações e biocombustíveis</span>  do que em 2012 para atender à demanda global <a href="https://www.fao.org/3/cb7654en/cb7654en.pdf" target="_blank">[1]</a>.</p>
+                        <p>Até 2050, a FAO estima que a agropecuária precisará produzir 50% mais alimentos para atender à demanda global.<a href="https://www.fao.org/3/cb7654en/cb7654en.pdf" target="_blank">[1]</a>.</p>
                     
-                        <p>Espera-se que o <span className="weight">consumo de alimentos</span> mais intensivos em água, como a <span className="weight">carne</span>, aumente <span className="weight">15%</span> até 2031 <a href="https://www.oecd-ilibrary.org/agriculture-and-food/oecd-fao-agricultural-outlook-2022-2031_f1b0b29c-en">[2]</a> .</p>
+                        <p>Espera-se que o consumo de alimentos mais intensivos em água, como as proteínas animais, aumente 15% até 2031 [2].<a href="https://www.oecd-ilibrary.org/agriculture-and-food/oecd-fao-agricultural-outlook-2022-2031_f1b0b29c-en" target="_blank">[2]</a> .</p>
 
-                        <p>Enquanto globalmente a <span className="weight">produção agrícola e pecuária</span>  demanda <span className="weight">72%</span> de toda a água retirada 
+                        <p>Globalmente a produção agropecuária demanda 72% de toda a água consumida no mundo
                         <a href="https://www.unwater.org/sites/default/files/app/uploads/2021/08/SDG6_Indicator_Report_642_Progress-on-Level-of-Water-Stress_2021_ENGLISH_pages-1.pdf" target="_blank" rel="noopener noreferrer">[3]</a>
-                        , sendo <span className="weight">41%</span>  destinado a <span className="weight">produção de alimentação animal </span> <a href="#" target="_blank" rel="noopener noreferrer">[4]</a> . Essa quantidade pode ser ainda maior nos países em desenvolvimento devido à intensificação da produção agrícola, aumentando a competição pela água </p>
+                        , sendo 41% disto consumido na produção de alimentos para os animais<a href="#" target="_blank" rel="noopener noreferrer">[4]</a> . Essa quantidade é ainda maior nos países em desenvolvimento onde a agropecuária representa grande parte da base da economia. </p>
 
-                        <p> <span className="weight">Estresse hídrico</span>   causado por secas intensas tem sido mais frequente devido as <span className="weight">mudanças climáticas.</span></p>
+                        <p>Eventos de escassez hídrica causados por secas intensas têm sido mais frequentes devido às mudanças climáticas.</p>
 
-                        <p>A  <span className="weight">falta de gestão hídrica</span> <span className="weight">na produção animal</span> pode gerar <span className="weight">perdas de produção.</span> </p>
+                        <p>A falta de gestão hídrica na produção animal pode gerar perdas de produção e prejuízos econômicos.</p>
 
-                        <p> O caminho para a <span className="weight">redução do estresse hídrico</span> passa por <span className="weight">sistemas alimentares sustentáveis e eficientes.</span> </p>
+                        <p>Devemos ter sistemas pecuários hidricamente eficientes.</p>
                     </div>
                 </div>
 
