90860843384998c7e32d26ccda02992b2ebfb662
Henrique Cardoso de Oliveira <henriqueau111@gmail.com>
2023-04-10 15:49:08 -0300

    modified:   src/components/Carousel/index.jsx 	modified:   src/components/Carousel/style.css

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,18 +2,7 @@ import "./style.css"
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
 import styled from "styled-components";
-import pig from "../../assets/svg/animals/pig-gc.jpg"
-import chicken from "../../assets/svg/animals/chicken.jpg"
 
-
-const Imgss = styled.img`
-    height: 100%;
-    min-height: 20vh;
-    max-height: 50vh;
-    background-size: cover;
-    background-position: 1rem 2rem;
-
-`
 
 const Section = styled.section`
     height: auto;
@@ -28,6 +17,20 @@ const Section = styled.section`
 
 `
 
+const Text = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: auto;
+    padding: 0.5rem;
+    margin-top: 1rem;
+    font-size: 1.4rem;
+`
+
+const BackgroundWhite = styled.div`
+    height: auto;
+    background: #fff;
+    padding: 1rem;
+`
 
 export function Caracteristica() {
 
@@ -40,43 +43,45 @@ export function Caracteristica() {
             <Carousel controls={true} indicators={false} className="carousel">
                 <Carousel.Item className="carouselItem">
 
-                    <div className="imgCow1">
-                    <figure className="figureImg">
+                    <div>
 
+                        <BackgroundWhite>
+                            <div className="imgCow1" />
+                        </BackgroundWhite>
 
-                    <figcaption>
+                        <Text>
     
-                        <p>Sistema à pasto: 1085 vacas em lactação</p>
+                            <p>Sistema à pasto: 1085 vacas em lactação</p>
 
-                        <p>Produção média de 5,055 litros de leite corrigido/cabeça.ano</p>
+                            <p>Produção média de 5,055 litros de leite corrigido/cabeça.ano</p>
 
-                        <p>Dieta (média): 93% volumoso; 7% concentrado</p>
+                            <p>Dieta (média): 93% volumoso; 7% concentrado</p>
 
-                        <p>Dieta: soja, milho</p>
+                            <p>Dieta: soja, milho</p>
 
-                        <p>Sistema semi-confinado: 246 vacas em lactação</p>
-                    </figcaption>
-                </figure>
+                            <p>Sistema semi-confinado: 246 vacas em lactação</p>
+                        </Text>
                     </div>
     
                 </Carousel.Item>
                 
                 <Carousel.Item interval={3500} className="carouselItem">
 
-                    <div className="imgPig1">
+                    <div>
 
-                    <figure className="figureImg">
+                        <BackgroundWhite>
+                            <div className="imgPig1" />
+                        </BackgroundWhite>
 
-                        <figcaption>
+                        <Text>
 
-                        <p>55.071 cabeças de suínos</p>
+                            <p>55.071 cabeças de suínos</p>
 
-                        <p>Peso carcaça suínos: 97,2kg</p>
+                            <p>Peso carcaça suínos: 97,2kg</p>
 
-                        <p>Dieta: soja, milho</p>
+                            <p>Dieta: soja, milho</p>
 
-                        </figcaption>
-                    </figure>
+                        </Text>
                     </div>
 
                     
@@ -85,17 +90,19 @@ export function Caracteristica() {
                 <Carousel.Item interval={3500} className="carouselItem">
 
 
-                    <div className="imgChicken1">
+                    <div>
 
-                    <figure className="figureImg">
+                        <BackgroundWhite>
+                            <div className="imgChicken1" />
+                        </BackgroundWhite>
 
-                        <figcaption>
-                            
+                        <Text>
+
                             <p>6.108,600 cabeças de suínos</p>
                             <p>Peso carcaça aves:2,2kg</p>
                             <p>Dieta: soja, milho</p>
-                        </figcaption>
-                    </figure>
+                     
+                        </Text>
                     </div>
                     
                 </Carousel.Item>
